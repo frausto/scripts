d11ad5879eacafe558854db3b38d764493fed2f4
nolan frausto <nrfrausto@gmail.com>
2011-12-26 01:52:49 -0600

    revertall checks to see if revert succeeds or not

--- revertall.sh
+++ revertall.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+function waitforfix {
+	while read input; do
+		if [ "${input}" == "revertall --continue" ]
+		then
+			break
+		fi
+	done
+}
+
 function revert() {
 	local grep="$1"
 	for sha in `git rev-list --all --grep $grep`
 	do
 		echo "reverting: $sha" 
-		git revert -n $sha
+		local result=`git revert -n $sha 2>&1`
+		if echo -e $result | grep -E '(error|fatal):'
+		then
+			echo -e "\nRevert Failed, please commit changes and type 'revertall --continue' to continue reverting"
+			waitforfix
+		fi
 		echo "reverted!" 
 	done	
 }
